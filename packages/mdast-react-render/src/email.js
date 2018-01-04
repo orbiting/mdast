7993a1f5ac31cef044a392c25b5c96a592cdfd87
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-01-04 15:31:15 +0100

    fix: Cannot read property 'doctype' of undefined

--- packages/mdast-react-render/src/email.js
+++ packages/mdast-react-render/src/email.js
@@ -10,7 +10,7 @@ export const Mso = ({children, gte}) =>
 
 const DEFAULT_DOCTYPE = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
 
-export const renderEmail = (mdast, schema, options) => {
+export const renderEmail = (mdast, schema, options = {}) => {
   const { doctype = DEFAULT_DOCTYPE } = options
   return (
     doctype +
